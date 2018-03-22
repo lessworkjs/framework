c010752c729fb21c2037901e7bd3ba12a216448f
William <gcphost@gmail.com>
2018-03-21 18:49:03 -0700

    App provider, lower case

--- Generators/templates/controller.tpl
+++ Generators/templates/controller.tpl
@@ -6,7 +6,7 @@ class <%= name %> extends require('../BaseController') {
       data: "Hello World!"
     };
 
-    use('Response').success(null, responseObject, results => {
+    use('response').success(null, responseObject, results => {
       return results.data;
     }, this.state.callback);
   }
---
+++ providers/AppProvider.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {
+  ServiceProvider
+} = require('adonis-fold');
+
+class AppProvider extends ServiceProvider {
+  /**
+   * Register method called by the Ioc container
+   * to register the provider
+   *
+   * @method register
+   *
+   * @return {void}
+   */
+  * register() {
+    this.app.singleton('Lesswork/App', function (app) {
+      const App = require('../lib/app');
+
+      return new App();
+    });
+  }
+}
+
+module.exports = AppProvider;
