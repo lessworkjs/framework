5e91a207dc9d6baebb93fc4c8d96c73925321cf7
William <gcphost@gmail.com>
2018-03-23 21:48:10 -0700

    work to backend

---
+++ Work/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function (app) {
+  const work = require('lesswork-cmd');
+
+  const Commands = require('..//Console');
+
+  const AppCommands = app.commands;
+
+  Object.keys(Commands).forEach((name) => {
+    work.addCommand(Commands[name]);
+  });
+
+  Object.keys(AppCommands).forEach((name) => {
+    work.addCommand(require(AppCommands[name]));
+  });
+
+  work.onError(function (error, commandName) {
+    const PrettyError = require('pretty-error');
+
+    console.error(new PrettyError().render(error));
+
+    process.exit(1);
+  });
+
+  work.wireUpWithCommander();
+
+  work.invoke();
+};
